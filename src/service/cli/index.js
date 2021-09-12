424662ad72e42988c5941de7a7f2d621c6fde8bd
Jenny Bykova <eva.gre4ka@yandex.ru>
2021-09-12 17:36:48 +0300

    Add constants

--- src/service/cli/index.js
+++ src/service/cli/index.js
@@ -2,10 +2,12 @@
 
 const version = require(`./versionjs`);
 const help = require(`./help.js`);
+const generate = require(`./generate.js`);
 
 const Cli = {
   [version.name]: version,
-  [help.name]: help
+  [help.name]: help,
+  [generate.name]: generate
 };
 
 module.exports = {
