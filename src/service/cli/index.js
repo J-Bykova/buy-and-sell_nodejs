2df347a34f6350f5a2b8cbe3d21d60b465b34cb9
Jenny Bykova <eva.gre4ka@yandex.ru>
2021-09-12 18:55:49 +0300

    Add mocks.json in git ignore

--- src/service/cli/index.js
+++ src/service/cli/index.js
@@ -2,10 +2,10 @@
 
 const version = require(`./version.js`);
 const help = require(`./help.js`);
-// const generate = require(`./generate.js`);
+const generate = require(`./generate.js`);
 
 module.exports.Cli = {
   [version.name]: version,
   [help.name]: help,
-  // [generate.name]: generate
+  [generate.name]: generate
 };
