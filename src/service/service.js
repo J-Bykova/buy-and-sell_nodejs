e8cd61b4b58bac31b1c857ef8b2f4b4d87225ccf
Jenny Bykova <eva.gre4ka@yandex.ru>
2021-09-12 18:49:14 +0300

    Add the help command

--- src/service/service.js
+++ src/service/service.js
@@ -11,12 +11,14 @@ const command = args[0];
 // Если команда не переданна
 if (!command) {
   console.log(`No command passed`);
+  Cli[`--help`].run();
   process.exit(1);
 }
 
 // Проверяем что есть обработчик для этой команды
 if (!Cli[command]) {
   console.log(`Unknown command: ${command}`);
+  Cli[`--help`].run();
   process.exit(1);
 }
 
