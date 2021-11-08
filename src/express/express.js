40e16b7f1d2fc6d75c8d4e1aba0e0b01873b805b
Jenny Bykova <eva.gre4ka@yandex.ru>
2021-11-09 00:25:52 +0300

    Set static folder

--- src/express/express.js
+++ src/express/express.js
@@ -1,10 +1,17 @@
 'use strict';
 
 const express = require(`express`);
+const path = require(`path`);
 
 const DEFAULT_PORT = 3000;
 const app = express();
 
-app.get(`/`, (req, res) => res.send(`Hello, Express.js!`));
-app.listen(DEFAULT_PORT,
-    () => console.log(`Сервер запущен на порту: ${DEFAULT_PORT}`));
+app.get(`/`, (req, res) => {
+  res.send(`<h1>Hello, Express.js!</h1>`);
+});
+
+app.use(express.static(path.join(__dirname, `../../`, `markup`)));
+
+app.listen(DEFAULT_PORT, () => {
+  console.log(`Сервер запущен на порту: ${DEFAULT_PORT}`);
+});
